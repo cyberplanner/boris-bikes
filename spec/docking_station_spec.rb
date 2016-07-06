534cae58e72743f070ef81e339ed1b9ca10fac1d
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-07-06 16:34:52 +0100

    incomplete dock method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,11 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
 end
