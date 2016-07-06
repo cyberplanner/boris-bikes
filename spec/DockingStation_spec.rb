029f3255212700cfc2c18895c9f27d3cfcec2899
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-06 16:17:40 +0100

    add bike to docking station

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,9 +1,18 @@
-#DockingStation_spec.rb
 require "docking_station"
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
+  it 'should dock bikes' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'check for docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
   it "release working bikes" do
     bike = subject.release_bike
