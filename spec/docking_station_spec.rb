e870f207a55ee61702b0cf3baa521d3259657d74
cyberplanner <saleh.gfx@gmail.com>
2016-07-06 17:08:03 +0100

    Complex Attributes, test passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,9 @@
 require 'docking_station'
 describe DockingStation do
-  
+
   describe '#release_bike' do
-	  it "Release bike method exists" do 
-	  	should respond_to(:release_bike) 
+	  it "Release bike method exists" do
+	  	should respond_to(:release_bike)
 	  end
 	  it "releases a bike if one is available" do
 	  	bike = Bike.new
@@ -23,7 +23,8 @@ describe DockingStation do
 
 	describe '#dock_bike' do
 		it "raises an error" do
-			subject.dock_bike(Bike.new)
+			#subject.dock_bike(Bike.new)
+      20.times {subject.dock_bike(Bike.new)}
 			expect{subject.dock_bike(Bike.new)}.to raise_error("Docking station full")
 		end
 	end
@@ -33,7 +34,7 @@ describe DockingStation do
   it "should see a docked bike" do
   	bike = Bike.new
   	subject.dock_bike(bike)
-  	expect(subject.bike).to eq bike
+  	expect(subject.bikes.last).to eq bike
   end
 
 end
