e0a13b1d998db0d8d1ecd789424ed3170dba87d4
Joe Sweeny <joesweeny84@hotmail.com>
2016-07-07 14:53:54 +0100

    checking there is a bike to release

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,18 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
+describe '#release_bike' do
+  it 'releases a bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.release_bike).to eq bike
+  end
+end
+
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
+    subject.dock_bike(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
 
